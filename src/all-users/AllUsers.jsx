97e9b32e280f73671190600498516e3d18ff8814
Sunil Paudel <mrsunilpaudel@gmail.com>
2022-01-12 15:45:55 +0545

    fix ui

--- src/all-users/AllUsers.jsx
+++ src/all-users/AllUsers.jsx
@@ -1,17 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table, TableBody, TableCell, TableHead, TableRow,makeStyles } from '@material-ui/core';
+import { getUsers } from '../services/API';
 
+const useStyle = makeStyles({
+    table:{
+        width:"80%",
+        margin:"50px 30px 10px 130px",
+    },
+    tableHeadCell:{
+        '& > *':{
+            background:"#000000",
+            color:"#FFFFFF",
+            fontSize:"20px",
+            fontWeight:"300"
+        }
+    },
+    tableBodyCell:{
+        '& > *':{
+            fontSize:"18px",
+        }
+    }
+})
 const AllUsers = () => {
+    const classes = useStyle();
+    const[users, setUsers] = useState([]);
+    
+    useEffect(()=>{
+        GetAllUsers();
+    },[]);
+
+    const GetAllUsers = async ()=>{
+        const resp = await getUsers();
+        console.log(resp.data);
+        setUsers(resp.data);
+    }
+
     return (
-        <div>
-            <h1>from AllUsers</h1>
-            <h1>from AllUsers</h1>
-            <h1>from AllUsers</h1>
-            <h1>from AllUsers</h1>
-            <h1>from AllUsers</h1>
-            <h1>from AllUsers</h1>
-            <h1>from AllUsers</h1>
-            <h1>from AllUsers</h1>
-        </div>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableRow className={classes.tableHeadCell}>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Username</TableCell>
+                        <TableCell>E-mail</TableCell>
+                        <TableCell>Phone Number</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                     users.map(userdata=>(
+                        <TableRow className={classes.tableBodyCell}>
+                            <TableCell>{userdata.id}</TableCell>
+                            <TableCell>{userdata.name}</TableCell>
+                            <TableCell>{userdata.username}</TableCell>
+                            <TableCell>{userdata.email}</TableCell>
+                            <TableCell>{userdata.phone}</TableCell>
+                        </TableRow>
+                     ))
+                   }
+                </TableBody>
+            </Table>
     )
 }
 
