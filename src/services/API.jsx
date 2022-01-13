02193405e5c337407706f0e8df8e106f89beffd7
Sunil Paudel <mrsunilpaudel@gmail.com>
2022-01-13 14:20:40 +0545

    delete and edit user api add

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -2,10 +2,17 @@ import axios from "axios";
 
 const URL= "http://localhost:3001/users";
 
-export const getUsers = async() =>{
-    return await axios.get(URL);
+export const getUsers = async(id) =>{
+    id=id || '';
+    return await axios.get(`${URL}/${id}`);
 }
 
 export const addUsers = async(user)=>{
     return await axios.post(URL,user);
+}
+export const editUsers = async(id,user)=>{
+    return await axios.put(`${URL}/${id}`,user);
+}
+export const deleteUser = async(id,user)=>{
+    return await axios.delete(`${URL}/${id}`);
 }
