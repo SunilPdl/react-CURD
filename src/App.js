b4e54c6c94d52dc4e741d00f5727ba8432e3edfb
Sunil Paudel <mrsunilpaudel@gmail.com>
2022-01-13 14:21:36 +0545

    editUser and pageNotFound component add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './navbar/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import UserNotFound from './all-users/UserNotFound';
+import EditUser from './editUser/EditUser';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Routes>
           <Route exact path='/' element={<AllUsers />} />
           <Route exact path='/add' element={<AddUsers />} />
-          <Route element={<UserNotFound /> } />
+          <Route exact path='/edit/:id' element={<EditUser/>} />
+          <Route path='*' element={<UserNotFound /> } />
         </Routes>
       </Router>
     </div>
